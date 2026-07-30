4d2b66bef8eb748d1d2aaa26518e320179ad4e82
Risa McNellis <risa.mcnellis@gmail.com>
2026-07-30 11:14:36 -0400

    Change showThumbnails setting to false

--- public/pasta.js
+++ public/pasta.js
@@ -43,7 +43,7 @@ const PASTA_CONFIG = {
    "showAbstracts": true, // true if we should show abstracts in search results
    "abstractLimit": 750, // Limit the number of characters in the abstract
    "showUserStoriesLink": true, // If false, do not display the user stories link for datasets
-   "showThumbnails": true, // If false, do not display dataset thumbnail images
+   "showThumbnails": false, // If false, do not display dataset thumbnail images
    "showBanner": false, // If false, the top banner will not be displayed
    "hideMapView": false, // true to hide the map view
    "facetVisibility": { // Facet visibility toggles
